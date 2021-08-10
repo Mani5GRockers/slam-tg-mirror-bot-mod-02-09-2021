b17d6858dcbc5e5f5b8a9b8561f36c4051ec855d
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-10 11:04:06 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -31,72 +31,46 @@ def stats(update, context):
     cpuUsage = psutil.cpu_percent(interval=0.5)
     memory = psutil.virtual_memory().percent
     disk = psutil.disk_usage('/').percent
-    stats = f'<b>╭──「  🚦 BOT STATS 🚦 」 </b>\n' \
-            f'<b>│</b>\n' \
-            f'<b>├  ⏰ Bot Uptime : {currentTime}</b>\n' \
-            f'<b>├  🔊 Start Time :</b> {current}\n' \
-            f'<b>├  🗄 Total Disk Space : {total}</b>\n' \
-            f'<b>├  🗂 Total Used Space : {used}</b>\n' \
-            f'<b>├  📂 Total Free Space : {free}</b>\n' \
-            f'<b>├  📑 Data Usage :</b>\n' \
-            f'<b>├  📥 Total Download : {recv}</b>\n' \
-            f'<b>├  📤 Total Upload : {sent}</b>\n' \
-            f'<b>├  🖥️ CPU : {cpuUsage}%</b>\n' \
-            f'<b>├  🚀 RAM : {memory}%</b>\n' \
-            f'<b>├  🗄 DISK : {disk}%</b>\n' \
-            f'<b>│</b>\n' \
-            f'<b>╰──「 Ⓜ️ @Mani5GRockers Ⓜ️ 」</b>'
-    update.effective_message.reply_photo(IMAGE_URL, stats, parse_mode=ParseMode.HTML)
+    stats = f'<b>Bot Uptime:</b> <code>{currentTime}</code>\n' \
+            f'<b>Total Disk Space:</b> <code>{total}</code>\n' \
+            f'<b>Used:</b> <code>{used}</code>  ' \
+            f'<b>Free:</b> <code>{free}</code>\n\n' \
+            f'<b>Upload:</b> <code>{sent}</code>\n' \
+            f'<b>Download:</b> <code>{recv}</code>\n\n' \
+            f'<b>CPU:</b> <code>{cpuUsage}%</code> ' \
+            f'<b>RAM:</b> <code>{memory}%</code> ' \
+            f'<b>DISK:</b> <code>{disk}%</code>'
     sendMessage(stats, context.bot, update)
 
 
 def start(update, context):
     start_string = f'''
 This bot can mirror all your links to Google Drive!
-
-👲 Modded By: @Mani5GRockers
-
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("🚦 Channel 🚦", "https://t.me/Mani5GRockers")
-    buttons.buildbutton("Ⓜ️ ᴍɪʀʀᴏʀ ɢʀᴏᴜᴘ Ⓜ️", "https://t.me/aws_public_chat")
-    buttons.buildbutton("⚙️ ᴘʀɪɪɪɪʏᴏ ʙᴏᴛꜱ ⚙️", "http://t.me/mani5grockersbot")
+    buttons.buildbutton("Repo", "https://github.com/breakdowns/slam-tg-mirror-bot")
+    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     LOGGER.info('UID: {} - UN: {} - MSG: {}'.format(update.message.chat.id, update.message.chat.username, update.message.text))
     uptime = get_readable_time((time.time() - botStartTime))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         if update.message.chat.type == "private" :
-             sendMessage(f"Hey I'm AWS MIRROR ZONE BOT\n\n➩ Developer by 🏆 Mani5GRockers 👨‍💻\n\n➩ /help\nSince: <code>{uptime}</code>", context.bot, update)
-     else :
+            sendMessage(f"Hey I'm Alive 🙂\nSince: <code>{uptime}</code>", context.bot, update)
+        else :
             sendMarkup(start_string, context.bot, update, reply_markup)
     else :
         sendMarkup(f"Oops! not a Authorized user.\nPlease deploy your own <b>slam-tg-mirror-bot</b>.", context.bot, update, reply_markup)
 
-@run_async
-def chat_list(update, context):
-    chatlist =''
-    chatlist += '\n'.join(str(id) for id in AUTHORIZED_CHATS)
-    sendMessage(f'<b>Authorized List:</b>\n{chatlist}\n', context.bot, update)
 
-
-@run_async
-def repo(update, context):
-    bot.send_message(update.message.chat_id,
-    reply_to_message_id=update.message.message_id,
-    text="🤷🤷🤷🤷", disable_web_page_preview=True)
-
-
-@run_async
 def restart(update, context):
     restart_message = sendMessage("Restarting, Please wait!", context.bot, update)
-    LOGGER.info(f'Restarting the Bot...')
     # Save restart message object in order to reply to it after restarting
+    with open(".restartmsg", "w") as f:
+        f.truncate(0)
+        f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
     fs_utils.clean_all()
-    with open('restart.pickle', 'wb') as status:
-        pickle.dump(restart_message, status)
-    execl(executable, executable, "-m", "bot")
-
+    os.execl(executable, executable, "-m", "bot")
 
 
 def ping(update, context):
@@ -113,89 +87,49 @@ def log(update, context):
 def bot_help(update, context):
     help_string_adm = f'''
 /{BotCommands.HelpCommand}: To get this message
-
 /{BotCommands.MirrorCommand} [download_url][magnet_link]: Start mirroring the link to Google Drive. Use /{BotCommands.MirrorCommand} qb to mirror with qBittorrent, and use /{BotCommands.MirrorCommand} qbs to select files before downloading
-
 /{BotCommands.TarMirrorCommand} [download_url][magnet_link]: Start mirroring and upload the archived (.tar) version of the download
-
 /{BotCommands.UnzipMirrorCommand} [download_url][magnet_link]: Starts mirroring and if downloaded file is any archive, extracts it to Google Drive
-
 /{BotCommands.CloneCommand} [drive_url]: Copy file/folder to Google Drive
-
 /{BotCommands.CountCommand} [drive_url]: Count file/folder of Google Drive Links
-
 /{BotCommands.DeleteCommand} [drive_url]: Delete file from Google Drive (Only Owner & Sudo)
-
 /{BotCommands.WatchCommand} [youtube-dl supported link]: Mirror through youtube-dl. Click /{BotCommands.WatchCommand} for more help
-
 /{BotCommands.TarWatchCommand} [youtube-dl supported link]: Mirror through youtube-dl and tar before uploading
-
 /{BotCommands.CancelMirror}: Reply to the message by which the download was initiated and that download will be cancelled
-
 /{BotCommands.CancelAllCommand}: Cancel all running tasks
-
 /{BotCommands.ListCommand} [search term]: Searches the search term in the Google Drive, If found replies with the link
-
 /{BotCommands.StatusCommand}: Shows a status of all the downloads
-
 /{BotCommands.StatsCommand}: Show Stats of the machine the bot is hosted on
-
 /{BotCommands.PingCommand}: Check how long it takes to Ping the Bot
-
 /{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
-
 /{BotCommands.UnAuthorizeCommand}: Unauthorize a chat or a user to use the bot (Can only be invoked by Owner & Sudo of the bot)
-
 /{BotCommands.AuthorizedUsersCommand}: Show authorized users (Only Owner & Sudo)
-
 /{BotCommands.AddSudoCommand}: Add sudo user (Only Owner)
-
 /{BotCommands.RmSudoCommand}: Remove sudo users (Only Owner)
-
 /{BotCommands.RestartCommand}: Restart the bot
-
 /{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
-
 /{BotCommands.ConfigMenuCommand}: Get Info Menu about bot config (Owner Only)
-
 /{BotCommands.UpdateCommand}: Update Bot from Upstream Repo (Owner Only)
-
 /{BotCommands.SpeedCommand}: Check Internet Speed of the Host
-
 /{BotCommands.ShellCommand}: Run commands in Shell (Terminal)
-
 /{BotCommands.ExecHelpCommand}: Get help for Executor module (Only Owner)
-
 /{BotCommands.TsHelpCommand}: Get help for Torrent search module
 '''
 
     help_string = f'''
 /{BotCommands.HelpCommand}: To get this message
-
 /{BotCommands.MirrorCommand} [download_url][magnet_link]: Start mirroring the link to Google Drive. Use /{BotCommands.MirrorCommand} qb to mirror with qBittorrent, and use /{BotCommands.MirrorCommand} qbs to select files before downloading
-
 /{BotCommands.TarMirrorCommand} [download_url][magnet_link]: Start mirroring and upload the archived (.tar) version of the download
-
 /{BotCommands.UnzipMirrorCommand} [download_url][magnet_link]: Starts mirroring and if downloaded file is any archive, extracts it to Google Drive
-
 /{BotCommands.CloneCommand} [drive_url]: Copy file/folder to Google Drive
-
 /{BotCommands.CountCommand} [drive_url]: Count file/folder of Google Drive Links
-
 /{BotCommands.WatchCommand} [youtube-dl supported link]: Mirror through youtube-dl. Click /{BotCommands.WatchCommand} for more help
-
 /{BotCommands.TarWatchCommand} [youtube-dl supported link]: Mirror through youtube-dl and tar before uploading
-
 /{BotCommands.CancelMirror}: Reply to the message by which the download was initiated and that download will be cancelled
-
 /{BotCommands.ListCommand} [search term]: Searches the search term in the Google Drive, If found replies with the link
-
 /{BotCommands.StatusCommand}: Shows a status of all the downloads
-
 /{BotCommands.StatsCommand}: Show Stats of the machine the bot is hosted on
-
 /{BotCommands.PingCommand}: Check how long it takes to Ping the Bot
-
 /{BotCommands.TsHelpCommand}: Get help for Torrent search module
 '''
 
