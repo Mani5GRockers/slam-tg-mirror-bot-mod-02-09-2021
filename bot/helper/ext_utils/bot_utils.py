11d31f00d23fd44a4bc17a83fb49213c67384de2
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-10 18:34:48 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -98,10 +98,11 @@ def get_progress_bar_string(status):
     p = min(max(p, 0), 100)
     cFull = p // 8
     cPart = p % 8 - 1
-    p_str = '🟩' * cFull
+    p_str = FINISHED_PROGRESS_STR * cFull
     if cPart >= 0:
-        p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += '🟩' * (PROGRESS_MAX_SIZE - cFull)
+        # p_str += PROGRESS_INCOMPLETE[cPart]
+        p_str += FINISHED_PROGRESS_STR
+    p_str += UNFINISHED_PROGRESS_STR * (PROGRESS_MAX_SIZE - cFull)
     p_str = f"[{p_str}]"
     return p_str
 
