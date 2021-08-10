708a8b2470996dbc465af91c6370000f4b90cca6
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-10 22:16:44 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -90,9 +90,9 @@ def update_all_messages():
             	    uldl_bytes += float(speedy.split('K')[0]) * 1024
                 elif 'MB/s' in speedy:
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
-        dlspeed = get_readable_file_size(dlspeed_bytes)
-        ulspeed = get_readable_file_size(uldl_bytes)
-        msg += f"\n<b>📀 FREE:</b> <code>{free}</code> | <b>⏰ UPTIME:</b> <code>{currentTime}</code>\n<b>📥 DL:</b> <code>{dlspeed}/s</code> 🔻 | <b>📤 UL:</b> <code>{ulspeed}/s</code> 🔺\n"
+        #dlspeed = get_readable_file_size(dlspeed_bytes)
+        #ulspeed = get_readable_file_size(uldl_bytes)
+        #msg += f"\n<b>📀 FREE:</b> <code>{free}</code> | <b>⏰ UPTIME:</b> <code>{currentTime}</code>\n<b>📥 DL:</b> <code>{dlspeed}/s</code> 🔻 | <b>📤 UL:</b> <code>{ulspeed}/s</code> 🔺\n"
     with status_reply_dict_lock:
         for chat_id in list(status_reply_dict.keys()):
             if status_reply_dict[chat_id] and msg != status_reply_dict[chat_id].text:
@@ -133,9 +133,9 @@ def sendStatusMessage(msg, bot):
             	    uldl_bytes += float(speedy.split('K')[0]) * 1024
                 elif 'MB/s' in speedy:
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
-        dlspeed = get_readable_file_size(dlspeed_bytes)
-        ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>📀 FREE:</b> <code>{free}</code> | <b>⏰ UPTIME:</b> <code>{currentTime}</code>\n<b>📥 DL:</b> <code>{dlspeed}/s</code> 🔻 | <b>📤 UL:</b> <code>{ulspeed}/s</code> 🔺\n"
+        #dlspeed = get_readable_file_size(dlspeed_bytes)
+        #ulspeed = get_readable_file_size(uldl_bytes)
+        #progress += f"\n<b>📀 FREE:</b> <code>{free}</code> | <b>⏰ UPTIME:</b> <code>{currentTime}</code>\n<b>📥 DL:</b> <code>{dlspeed}/s</code> 🔻 | <b>📤 UL:</b> <code>{ulspeed}/s</code> 🔺\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:
