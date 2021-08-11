819dc4568c9cb596e954c2c02e14962adc9156ae
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-12 00:00:51 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -289,7 +289,8 @@ def _mirror(bot, update, isTar=False, extract=False):
                         link = file.get_file().file_path
 
     if not bot_utils.is_url(link) and not bot_utils.is_magnet(link):
-    sendMessage('<b>❌ Opps 🤦! Not Like That</b>\n\nCheck 👉 <a href="https://awslink.in/awsmirrorzonehelp"> 𝐇𝐎𝐖 𝐓𝐎 𝐌𝐈𝐑𝐑𝐎𝐑?</a>', bot, update)
+          sendMessage('No download source provided', bot, update)
+          sendMessage('❌ Opps 🤦! Not Like That\n\n𝐇𝐎𝐖 𝐓𝐎 𝐌𝐈𝐑𝐑𝐎𝐑\n\n📝 Check 👉 https://awslink.in/awsmirrorzonehelp/n/n', bot, update)
     return
 
     try:
