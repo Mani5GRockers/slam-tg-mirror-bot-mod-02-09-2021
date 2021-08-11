25d5abf4d6ffb4540f688f753ef6a3d5f1ffe09a
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-11 17:50:45 +0530

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -17,11 +17,11 @@ def _watch(bot: Bot, update, isTar=False):
     try:
         link = message_args[1]
     except IndexError:
-        msg = f"/{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] to mirror with youtube-dl.\n\n"
-        msg += "<b>Note: Quality and custom name are optional</b>\n\nExample of quality: audio, 144, 240, 360, 480, 720, 1080, 2160."
-        msg += "\n\nIf you want to use custom filename, enter it after |"
-        msg += f"\n\nExample:\n<code>/{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |file name</code>\n\n"
-        msg += "This file will be downloaded in 720p quality and it's name will be <b>file name</b>"
+        msg = f"\n✥════ @Mani5GRockers ════✥\n\n👉 /{BotCommands.WatchCommand} [youtube-dl supported link] [quality] |[CustomName] to mirror with youtube-dl.\n\n"
+        msg += "<b>✥ Note: Quality and custom name are optional</b>\n\nExample of quality: audio, 144, 240, 360, 480, 720, 1080, 2160."
+        msg += "\n\n✥ If you want to use custom filename, enter it after |"
+        msg += f"\n\n✥ Example:\n\n<code>👉 /{BotCommands.WatchCommand} https://youtu.be/Pk_TthHfLeE 720 |file name</code>\n\n"
+        msg += "✥ This file will be downloaded in 720p quality and it's name will be <b>file name \n\n✥════ @Mani5GRockers ════✥</b>"   
         sendMessage(msg, bot, update)
         return
     
