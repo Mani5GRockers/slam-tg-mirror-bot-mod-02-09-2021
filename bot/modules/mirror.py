380e7049bae9e7d6182efe929ffb694f532e9ebe
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-12 09:15:37 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -191,7 +191,7 @@ class MirrorListener(listeners.MirrorListeners):
             else:
                 uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
             if uname is not None:
-                msg += f'\n\ncc: {uname}'
+             msg += f'\n\n<b>👤 Uploader: </b>👉 {uname}\n\n✅ #Uploaded To AWS Mirror Drive ✓ \n\n⛔ 𝘿𝙤 𝙣𝙤𝙩 𝙨𝙝𝙖𝙧𝙚 𝙄𝙣𝙙𝙚𝙭 𝙇𝙞𝙣𝙠🙂 \n\n✥════ @Mani5GRockers ════✥'</b>'
             try:
                 fs_utils.clean_download(download_dict[self.uid].path())
             except FileNotFoundError:
