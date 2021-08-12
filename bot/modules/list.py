5c489e187b18c8a980a2f7a7c99515e5c9bd03ba
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-13 00:51:58 +0530

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -10,15 +10,15 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 def list_drive(update, context):
     try:
         search = update.message.text.split(' ',maxsplit=1)[1]
-        LOGGER.info(f"Searching: {search}")
-        reply = sendMessage('Searching..... Please wait!', context.bot, update)
+        LOGGER.info(f"🔎 Searching: {search}")
+        reply = sendMessage('🔎 Searching..... Please wait!', context.bot, update)
         gdrive = GoogleDriveHelper(None)
         msg, button = gdrive.drive_list(search)
 
         if button:
             editMessage(msg, reply, button)
         else:
-            editMessage(f'No result found for <code>{search}</code>', reply, button)
+            editMessage(f'❗ No result found for <code>{search}</code>', reply, button)
 
     except IndexError:
         sendMessage('Send a search key along with command', context.bot, update)
