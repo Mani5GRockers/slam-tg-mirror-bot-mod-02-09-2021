03d3c314627e28b2ceedd5f6582ea7ebe88a92fc
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-13 19:44:30 +0530

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -21,7 +21,7 @@ def list_drive(update, context):
             editMessage(f'❗ No result found for <code>{search}</code>', reply, button)
 
     except IndexError:
-        sendMessage('Send a search key along with command', context.bot, update)
+        sendMessage('Send a search name along with command', context.bot, update)
 
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_drive, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
