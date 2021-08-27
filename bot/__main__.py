825b72b55e211927e73643ecb966c972db3d7d2f
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-27 15:24:31 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -206,23 +206,23 @@ def bot_help(update, context):
 
 botcmds = [
         (f'{BotCommands.HelpCommand}','Get Detailed Help'),
-        (f'{BotCommands.MirrorCommand}', 'New User Start Mirroring'),
-        (f'{BotCommands.TarMirrorCommand}','Start mirroring Magnet link, Direct link, Telegram files .zip and .tar more etc.'),
-        (f'{BotCommands.UnzipMirrorCommand}','Extract files'),
-        (f'{BotCommands.CloneCommand}','Copy file/folder to Drive'),
-        (f'{BotCommands.CountCommand}','Count file/folder of Drive link'),
+        (f'{BotCommands.MirrorCommand}', 'New User Start XXX Mirroring'),
+        (f'{BotCommands.TarMirrorCommand}','Start XXX mirroring Magnet link, Direct link, Telegram files .zip and .tar more etc.'),
+        (f'{BotCommands.UnzipMirrorCommand}','XXX Extract files'),
+        (f'{BotCommands.CloneCommand}','XXX Copy file/folder to Drive'),
+        (f'{BotCommands.CountCommand}','XXX Count file/folder of Drive link'),
         (f'{BotCommands.DeleteCommand}','Delete file from Drive'),
         (f'{BotCommands.WatchCommand}','Mirror Youtube-dl support link'),
         (f'{BotCommands.TarWatchCommand}','Mirror Youtube playlist link as .tar or .zip'),
         (f'{BotCommands.CancelMirror}','Cancel a task'),
         (f'{BotCommands.CancelAllCommand}','Cancel all tasks'),
-        (f'{BotCommands.ListCommand}','Searches files in Drive'),
-        (f'{BotCommands.StatusCommand}','Get Mirror Status message'),
-        (f'{BotCommands.StatsCommand}','Bot Usage Stats'),
+        (f'{BotCommands.ListCommand}','XXX Searches files in Drive'),
+        (f'{BotCommands.StatusCommand}','Get XXX Mirror Status message'),
+        (f'{BotCommands.StatsCommand}','XXX Bot Usage Stats'),
         (f'{BotCommands.PingCommand}','Ping the Bot'),
         (f'{BotCommands.RestartCommand}','Restart the bot [owner/sudo only]'),
         (f'{BotCommands.LogCommand}','Get the Bot Log [owner/sudo only]'),
-        (f'{BotCommands.TsHelpCommand}','Torrent Search Name')
+        (f'{BotCommands.TsHelpCommand}','Torrent Search Name XXX')
     ]
 
 
