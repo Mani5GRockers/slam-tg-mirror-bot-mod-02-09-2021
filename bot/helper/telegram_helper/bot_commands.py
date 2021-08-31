34240aa5c4d829bf476c7f0d03f4469543be1d9e
mani66660 <42831370+mani66660@users.noreply.github.com>
2021-08-31 21:27:01 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,7 +1,7 @@
 class _BotCommands:
     def __init__(self):
         self.StartCommand = 'start'
-        self.MirrorCommand = 'mirror'
+        self.MirrorCommand = 'seed'
         self.UnzipMirrorCommand = 'unzip'
         self.TarMirrorCommand = 'tar'
         self.CancelMirror = 'cancel'
